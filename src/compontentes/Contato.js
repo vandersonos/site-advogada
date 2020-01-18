ba00dae1e7e2bcad1fb020071019162577b741a2
Vanderson <vanderson.th@gmail.com>
2020-01-18 19:33:25 +0000

    ajuste no layout de contato

--- src/compontentes/Contato.js
+++ src/compontentes/Contato.js
@@ -14,7 +14,7 @@ class Contato extends React.Component{
         window.open(target);
         
     }
-    enviarMensagemMessenger(){
+    enviarMensagemEmail(){
         window.open('https://www.messenger.com/t/taianemartinscosta');
     }
     validaForm(){
@@ -35,39 +35,70 @@ class Contato extends React.Component{
             <div class="col">
                 <div id={this.props.id} class="section scrollspy">
                     <div class="card-panel grey lighten-5 z-depth-1">
-                        <div class="row">
-                            <div class="col s12 "><h5>Contato</h5></div>
-                            <div class="col s12 ">
-                                <form class="col s12 m6">
-                                    <div class="row">
-                                        <div class="input-field col s12"><i class="material-icons prefix">account_circle</i>
-                                            <input id="nome" type="text" class="validate"/>
-                                            <label for="nome">Nome</label>
-                                        </div>
-                                        <div class="input-field col s12"><i class="material-icons prefix">phone</i>
-                                            <input id="telefone" type="tel" class="validate"/>
-                                            <label for="telefone">Telefone</label>
-                                        </div>
-                                        <div class="input-field col s12"><i class="material-icons prefix">question_answer</i>
-                                            <textarea id="mensagem" class="materialize-textarea"></textarea>
-                                            <label for="mensagem">Mensagem</label>
-                                        </div>
+                        <div class="row">                              
+                            <form class="col s12 m6 form-contato">
+                                    <div class="col s12">
+                                        <h5 class="valign-wrapper">Contato</h5>
+                                    </div>
+                                    <div class="input-field col s12"><i class="material-icons prefix">account_circle</i>
+                                        <input id="nome" type="text" class="validate"/>
+                                        <label for="nome">Nome</label>
+                                    </div>
+                                    <div class="input-field col s12"><i class="material-icons prefix">phone</i>
+                                        <input id="telefone" type="tel" class="validate"/>
+                                        <label for="telefone">Telefone</label>
+                                    </div>
+                                    <div class="input-field col s12"><i class="material-icons prefix">question_answer</i>
+                                        <textarea id="mensagem" class="materialize-textarea"></textarea>
+                                        <label for="mensagem">Mensagem</label>
                                     </div>
                                 </form>
-                            <div class="col s12 m6">
+                            <div class="form-contato-acoes col s12 m6">
                                 <div class="col s12">
-                                    <h5 class="valign-wrapper">Escolha abaixo a melhor forma de me contatar</h5>
+                                    <h6 class="valign-wrapper">Escolha abaixo a melhor forma de me contatar</h6>
                                 </div>
                                 <div class="input-field col s12">
-                                    <button class="btn waves-effect waves-light" onClick={(e) => this.enviarMensagemWhatsapp(e)}>
-                                        Enviar via whatsapp<i class="material-icons right">send</i>
-                                    </button>
+                                    <div class='row'>
+                                        <div class='col'><img src='img/whatsapp.png'/></div>
+                                        <div class='col'>
+                                            <button class="btn waves-effect waves-light" onClick={(e) => this.enviarMensagemWhatsapp(e)}>
+                                                Enviar whatsapp<i class="material-icons right hide-on-small-only">send</i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class='row'>
+                                        <div class='col'>
+                                            <img src='img/email.png'/>
+                                        </div>
+                                        <div class='col'>
+                                            <button class="btn waves-effect waves-light" onClick={(e) => this.enviarMensagemEmail(e)}>
+                                                Enviar email<i class="material-icons right hide-on-small-only">send</i>
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="input-field col s12">
-                                    <button class="btn waves-effect waves-light" type="button" >Enviar via facebook mensenger<i class="material-icons right">send</i></button>
+                                <div class="col s12 form-title-pagina-facebook">
+                                    <h6 class="valign-wrapper">Ou acesse minha página no facebook</h6>
                                 </div>
+                                <div class="col s12">
+                                    <div class='row'>
+                                        <div class='col'>   
+                                            <a target='_blank' href='https://www.facebook.com/taianemartinscosta/'>
+                                            <img src='img/facebook.png'/>
+                                            </a>
+                                        
+                                        </div>
+                                        <div class='col'>
+                                            <a target='_blank' href='https://www.facebook.com/taianemartinscosta/'>
+                                            <button class="btn waves-effect waves-light" >
+                                                    Acessar página<i class="material-icons right  hide-on-small-only"></i>
+                                                </button>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+    
                             </div>
-                        </div>
                         </div>
                     </div>
                 </div>
