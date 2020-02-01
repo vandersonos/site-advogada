1e5d7799e5f69880cf2d87c3aedee13dd91f3329
Vanderson <vanderson.th@gmail.com>
2020-02-01 02:56:24 +0000

    versao build

---
+++ build/precache-manifest.e8b9e573e7e5e6f11278ec1fa7069727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad00d08bfc20cd76f4563675ffd75dba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d426ba84c5420090a4ef",
+    "url": "/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "8c037636c9839ae19f9c",
+    "url": "/static/css/main.c46a4d1d.chunk.css"
+  },
+  {
+    "revision": "d426ba84c5420090a4ef",
+    "url": "/static/js/2.15d218b4.chunk.js"
+  },
+  {
+    "revision": "8c037636c9839ae19f9c",
+    "url": "/static/js/main.9df43a24.chunk.js"
+  },
+  {
+    "revision": "19d590042be05d94698e",
+    "url": "/static/js/runtime-main.9bf3308e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
